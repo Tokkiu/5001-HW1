c695a609264935600ce07427b153b6d866be13a1
高璟琦 <mrgao.ary@gmail.com>
2020-10-19 15:56:28 +0800

    add nano second

--- 5001-1.sh
+++ 5001-1.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-for i in $(seq 1 100)
+for i in $(seq 1 1)
 do
    mkdir DDM${i}
-   date > DDM${i}/time_till_now.txt
+   echo 'nanoseconds since 1970-01-01 00:00:00 UTC:' >> DDM${i}/time_till_now.txt
+   date +%s%N  >> DDM${i}/time_till_now.txt
 done
